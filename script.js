e6efe3f7bef7de2b741aa11b6615acbf697996a3
ahmad <abdulkareemahmad413@gmail.com>
2024-07-31 12:16:06 +0100

    added a dropdown effect

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 const hamburger = document.querySelector (".hamburger");
 const cancel = document.querySelector (".cancel");
 const nav = document.querySelector (".navList ul");
+const navList = document.querySelector (".navList ul li");
 const join = document.querySelector (".joinBtn");
 const signIn = document.querySelector (".signIn");
+const turnRight = document.querySelector(".fa-caret-down");
 const company = document.querySelector (".company");
 const help = document.querySelector (".help");
-const turnRight = document.querySelector(".fa-caret-down");
+const companyLists = document.querySelector(".company ul");
+const helpLists = document.querySelector(".help ul");
+// const backgroundOverlay1 = document.querySelector(".overlay1");
+// const backgroundOverlay2 = document.querySelector(".overlay2");
 
 // if (window.matchMedia("(max-width:767px)").matches){
 //     turnRight.classList.add("fa-caret-right");
 // } else {
 //     turnRight.classList.remove("fa-caret-down");
 // }
+company.addEventListener('mouseenter', function(){
+    companyLists.style.display = "block";
+    // backgroundOverlay1.style.display = "block";
+});
+
+company.addEventListener('mouseleave', function(){
+    companyLists.style.display = "none";
+    // backgroundOverlay1.style.display = "none";
+});
+
+help.addEventListener('mouseenter', function(){
+    helpLists.style.display = "block";
+    // backgroundOverlay2.style.display = "block";
+});
+
+help.addEventListener('mouseleave', function(){
+    helpLists.style.display = "none";
+    // backgroundOverlay2.style.display = "none";
+});
+
+// companyLists.addEventListener('mouseleave', function(){
+//     companyLists.style.display = "none";
+//     backgroundOverlay1.style.display = "none";
+// });
+
+// navList.addEventListener('mouseleave', function(){
+//     helpLists.style.display = "none";
+//     backgroundOverlay2.style.display = "none";
+// });
+
+
 
 
 hamburger. addEventListener('click', function (){
